59b78369afa258ffbf17f58f5d89a7545bf5efea
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-14 15:10:40 +0300

    Created select_folder_page.dart

---
+++ lib/select_folder_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectFolderPage extends StatelessWidget {
+  const SelectFolderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text("select_folder_page"),
+    );
+  }
+}
