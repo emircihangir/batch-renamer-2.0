0ce85048d98218ca310371d20b5c3604c40b44ef
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-15 16:10:02 +0300

    Added the select folder button

--- lib/select_folder_page.dart
+++ lib/select_folder_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SelectFolderPage extends StatelessWidget {
   const SelectFolderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("select_folder_page"),
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: MediaQuery.of(context).size.height * 0.5,
+          child: InkWell(
+            onTap: () {},
+            child: SvgPicture.asset(
+              "assets/select_folder_illustration.svg",
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
