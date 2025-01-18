d1679ea40faaa27a1017a118038bb85abf667c0f
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-01-18 22:58:04 +0300

    Switched to MacOS design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 //* Pages
 import 'select_folder_page.dart';
 
-void main() {
+/// This method initializes macos_window_utils and styles the window.
+Future<void> _configureMacosWindowUtils() async {
+  const config = MacosWindowUtilsConfig();
+  await config.apply();
+}
+
+Future<void> main() async {
+  await _configureMacosWindowUtils();
   runApp(const MyApp());
 }
 
@@ -12,10 +21,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white, appBarTheme: AppBarTheme(backgroundColor: Colors.white, surfaceTintColor: Colors.blue.shade200, iconTheme: IconThemeData(color: Colors.blue))),
+    return MacosApp(
       debugShowCheckedModeBanner: false,
-      home: const SelectFolderPage(),
+      home: MacosWindow(
+        child: MacosScaffold(
+          toolBar: ToolBar(
+            actions: [
+              ToolBarIconButton(
+                label: "Select Files",
+                tooltipMessage: "Select Files",
+                icon: const MacosIcon(CupertinoIcons.folder_badge_plus),
+                showLabel: false,
+                onPressed: () {},
+              )
+            ],
+          ),
+          children: [
+            ContentArea(
+              builder: (context, scrollController) {
+                return Text("asd");
+              },
+            )
+          ],
+        ),
+      ),
+      theme: MacosThemeData.light(),
+      darkTheme: MacosThemeData.dark(),
+      themeMode: ThemeMode.system,
     );
   }
 }
--- lib/select_folder_page.dart
+++ lib/select_folder_page.dart
@@ -5,31 +5,27 @@ import 'package:batchrenamerv2/rename_files_page.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 class SelectFolderPage extends StatelessWidget {
   const SelectFolderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.5,
-          height: MediaQuery.of(context).size.height * 0.5,
-          child: InkWell(
-            onTap: () async {
+    return MacosWindow(
+      child: Center(
+        child: PushButton(
+            secondary: true,
+            controlSize: ControlSize.large,
+            onPressed: () async {
               FilePickerResult? selectedFiles = await FilePicker.platform.pickFiles(allowMultiple: true);
 
               if (selectedFiles != null) {
                 List<File> files = selectedFiles.paths.map((path) => File(path!)).toList();
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) => RenameFilesPage(initialValue: files)));
+                if (context.mounted) Navigator.of(context).push(MaterialPageRoute(builder: (context) => RenameFilesPage(initialValue: files)));
               }
             },
-            child: SvgPicture.asset(
-              "assets/select_folder_illustration.svg",
-            ),
-          ),
-        ),
+            child: const Text("Select Files")),
       ),
     );
   }
